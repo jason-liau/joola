c3e28a3d798e73551d8b639f1a69f7e24cefde6d
Jason Liau <jasonbenjaminliau@gmail.com>
2024-06-28 22:05:14 -0400

    update nav bars

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -16,48 +16,77 @@ class _HomePageState extends State<HomePage> {
     FirebaseAuth.instance.signOut();
   }
 
+  int index = 0;
+
+  void _navigateToScreens(i) {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(actions: [IconButton(onPressed: signOut, icon: const Icon(Icons.settings_outlined))]),
       bottomNavigationBar: Container(
         color: Colors.black,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 5
-          ),
-          child: GNav(
-            backgroundColor: Colors.black,
-            color: Colors.grey.shade600,
-            activeColor: Colors.white,
-            gap: 8,
-            onTabChange: (index) {
-            },
-            tabs: const [ 
-              GButton(
-                icon: Icons.home_outlined,
-                text: 'Home'
-              ),
-              GButton(
-                icon: Icons.sports_tennis_outlined,
-                text: 'Train'
-              ),
-              GButton(
-                icon: Icons.favorite_border_outlined,
-                text: 'Welness'
-              ),
-              GButton(
-                icon: Icons.feed_outlined,
-                text: 'Services'
-              ),
-              GButton(
-                icon: Icons.account_circle_outlined,
-                text: 'Profile'
-              ),
-            ]
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: BottomNavigationBar(
+              showUnselectedLabels: false,
+              backgroundColor: Colors.black,
+              selectedItemColor: Colors.white,
+              currentIndex: index,
+              onTap: (int index) {
+                setState(() {
+                  this.index = index;
+                });
+                _navigateToScreens(index);
+              },
+              type: BottomNavigationBarType.fixed,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home_outlined,
+                    color: Colors.grey,
+                  ),
+                  activeIcon: Icon(Icons.home),
+                  label: 'Home'
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.sports_tennis_outlined,
+                    color: Colors.grey,
+                  ),
+                  activeIcon: Icon(Icons.sports_tennis),
+                  label: 'Train'
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.favorite_border_outlined,
+                    color: Colors.grey,
+                  ),
+                  activeIcon: Icon(Icons.favorite),
+                  label: 'Wellness'
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.feed_outlined,
+                    color: Colors.grey,
+                  ),
+                  activeIcon: Icon(Icons.feed),
+                  label: 'Services'
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.account_circle_outlined,
+                    color: Colors.grey,
+                  ),
+                  activeIcon: Icon(Icons.account_circle),
+                  label: 'Profile'
+                ),
+              ]
+            ),
           ),
         ),
-      )
     );
   }
 }
